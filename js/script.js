e88c77640f5d644afcc6190fa95ba3ce92c86f5c
newsLineProject <katya_aksenchik@tut.by>
2017-01-31 16:16:57 +0300

    СSS-изменения
    Изменеия фона, исправление недочетов

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 /**
  * Created by Katsiaryna on 27/01/2017.
  */
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".log-in-button").click(function(){
-        var top=$(this).position().top;
-        $(".log-in-pop-up").css({top: top+25, right: 0}).toggle();
+    $(".log-in-button").click(function () {
+        var top = $(this).position().top;
+        $(".log-in-pop-up").css({top: top + 25, right: 0}).toggle();
     });
 
 });
+
+
 
